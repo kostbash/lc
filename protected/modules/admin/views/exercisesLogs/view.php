fed0b356c5cf879ef2bf5cca0b4e8ab3bd044167
FameBoss <famelink@mail.ru>
2014-11-28 23:36:14 +0400

    1212

--- protected/modules/admin/views/exercisesLogs/view.php
+++ protected/modules/admin/views/exercisesLogs/view.php
@@ -4,7 +4,7 @@
         <div class="exercise clearfix">
             <div class="question"><b>Условие задания:</b> <?php echo "$exercise->condition"; ?></div>
             <div class="answer clearfix">
-                <?php if($exercise->id_visual) $this->renderPartial("/visualizations/{$exercise->id_visual}", array('model'=>$exercise, 'answers'=>$model->answerUnserialize)); ?>
+                <?php if($exercise->id_visual) $this->renderPartial("visualizations/{$exercise->id_visual}", array('model'=>$exercise, 'answers'=>$model->answerUnserialize)); ?>
             </div>
 
             <div class="result color-<?php if(!$model->right) echo 'un'; ?>right"><?php echo $model->right ? "Верно" : "Не верно"; ?></div>
