ef41d2787a340785a927be83956171c018885c25
fameBoss <famelink@mail.ru>
2015-01-15 14:36:07 +0300

    545

---
+++ protected/migrations/m150115_113111_change_base.php
@@ -0,0 +1,33 @@
+<?php
+
+function transformationFormat($matches)
+{
+    return "[".$matches[1].":{".$matches[2]."}]";
+}
+
+class m150115_113111_change_base extends CDbMigration
+{
+	public function up()
+	{
+            set_time_limit(60000);
+
+            $answers = ExercisesListOfAnswers::model()->findAll();
+            foreach($answers as $answer)
+            {
+                $answer->answer = preg_replace_callback("#//([\d\w]*)=\((.*)\)//#uUm", "transformationFormat", $answer->answer);
+                $answer->save(false);
+            }
+
+            $exercises = Exercises::model()->findAll();
+            foreach($exercises as $exercise)
+            {
+                $exercise->condition =  preg_replace_callback("#//([\d\w]*)=\((.*)\)//#uUm", "transformationFormat", $exercise->condition);
+                $exercise->save(false);
+            }
+	}
+
+	public function down()
+	{
+            
+	}
+}
